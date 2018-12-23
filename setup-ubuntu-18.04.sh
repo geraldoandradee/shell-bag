f47e54601bf8fdb3234db5050ea83853b4a7224c
Geraldo Andrade <geraldo@geraldoandrade.com>
2018-12-23 18:56:38 -0200

    Added forgotten docker-compose

--- setup-ubuntu-18.04.sh
+++ setup-ubuntu-18.04.sh
@@ -77,6 +77,8 @@ install_developer_things() {
   add-apt-repository -y -u -s "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   apt install docker-ce -y
   docker run hello-world
+  curl -L https://github.com/docker/compose/releases/download/VERSION_NUM/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
   
   if [ -z "$COMMON_USER" ]
   then
