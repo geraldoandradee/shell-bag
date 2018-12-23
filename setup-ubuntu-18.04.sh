222b59a7b67c521eebb21b5983e3c591fdc1a39c
Geraldo Andrade <geraldo@geraldoandrade.com>
2018-12-23 18:59:35 -0200

    Fix docker-compose version

--- setup-ubuntu-18.04.sh
+++ setup-ubuntu-18.04.sh
@@ -77,7 +77,7 @@ install_developer_things() {
   add-apt-repository -y -u -s "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   apt install docker-ce -y
   docker run hello-world
-  curl -L https://github.com/docker/compose/releases/download/VERSION_NUM/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
   
   if [ -z "$COMMON_USER" ]
