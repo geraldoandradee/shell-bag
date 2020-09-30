f6b21e899080a3ca5eaa30b7d3c1856a00a280d9
Geraldo Andrade <geraldo@geraldoandrade.com>
2020-09-30 09:49:40 +0200

    Update Chrome OS linux setup update

--- setup-chrome-os-85.sh
+++ setup-chrome-os-85.sh
@@ -1,31 +1,90 @@
 #!/usr/bin/env bash 
 
-sudo apt update
-sudo apt upgrade -y
+# Constants
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;93m'
+
+info() {
+ echo "${BLUE}$1${NC}" 
+}
+
+error() {
+  echo "${RED}$1${NC}" 
+}
+
+warn() {
+  echo "${YELLOW}$1${NC}" 
+}
+
+success() {
+  echo "${GREEN}$1${NC}" 
+}
+
+check_root() {
+  if [ $(whoami) != "root" ]
+  then
+    info ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+    error "######### This script must be executed as root! #########"
+    info ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+    exit 1
+  fi
+}
+
+
+info ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+info "::::::::: Setup Script For Linux/Chrome OS 85 :::::::::::"
+info "::::::::::::: Debian GNU/Linux 10 (buster) ::::::::::::::" 
+info ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+info ""
+info ""
+info "This setup is meant for developers, so if you're not a "
+info "developer do not use it"
+info ""
+info ""
+
+check_root
+
+info "Type the name of your user (not root), followed by [ENTER]:"
+read COMMON_USERNAME
+info "Github/Docker configurations will be applied to $COMMON_USERNAME"
+
+info "Type your name (i.e. hi@geraldoandrade.com), followed by [ENTER]:"
+read COMMON_USER_EMAIL
+info "Github configurations will be applied to $COMMON_USER_EMAIL"
+
+info "Type your email (i.e. Geraldo Andrade), followed by [ENTER]:"
+read COMMON_USER_NAME
+info "Github configurations will be applied to $COMMON_USER_NAME"
+
+apt update
+apt upgrade -y
 
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+bash -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code # or code-insiders
+apt install apt-transport-https -y
+apt update
+apt install code -y # or code-insiders
 
 echo "Installing NODE"
-sudo apt install curl software-properties-common
+apt install curl software-properties-common -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
 sudo apt install nodejs -y
 sudo npm install wscat yo -g
 
 echo "Installing Docker"
-sudo apt remove docker docker-engine docker.io containerd runc -y
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+apt remove docker docker-engine docker.io containerd runc -y
+apt update
+apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
-sudo usermod -aG docker $USER
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-compose -y
+usermod -aG docker $COMMON_USERNAME
 
 echo "Installing Google Cloud SDK"
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
@@ -56,3 +115,15 @@ brew install gcc
 
 echo Installing GoLang
 brew install go
+
+
+echo Setting up github
+git config --global user.email "$COMMON_USER_EMAIL"
+git config --global user.name "$COMMON_USER_NAME"
+
+
+info ""
+info ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+success ":::::::::: Setup for $(lsb_release -d -s) has DONE! :::::::"
+info ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+info ""
